28c3e0f678b458545d7eb06552b082b753f2762e
Onur Köse <kose.onur@gmail.com>
2017-12-26 20:22:29 +0200

    Update SNMPWrapper.php

--- src/SNMPWrapper.php
+++ src/SNMPWrapper.php
@@ -1,9 +1,29 @@
 <?php
 namespace OnurKose\SNMPWrapper;
 
-use Nelisys\Snmp as BaseSNMP;
+use Exception;
+use Nelisys\Snmp;
 
-class SNMPWrapper extends BaseSNMP
+class SNMPWrapper
 {
-    //
+    protected $instance;
+
+    public function __construct($host, $community)
+    {
+        try {
+            $this->instance = new Snmp($host, $community);
+        } catch (Exception $e) {
+            throw new Exception('Error Processing Request', 1);
+        }
+    }
+    
+    public function get($oids)
+    {
+        return $this->instance->get($oids);
+    }
+    
+    public function walk($oid)
+    {
+        return $this->instance->walk($oid);
+    }
 }
