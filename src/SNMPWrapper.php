2e09d48d7966fac897187acd34913c39464f273c
Onur Köse <kose.onur@gmail.com>
2017-12-26 20:46:43 +0200

    Update SNMPWrapper.php

--- src/SNMPWrapper.php
+++ src/SNMPWrapper.php
@@ -8,20 +8,29 @@ class SNMPWrapper
 {
     protected $instance;
 
-    public function __construct($host, $community)
+    public function __construct()
     {
         try {
-            $this->instance = new Snmp($host, $community);
+            $this->instance = new Snmp('127.0.0.1', 'public');
         } catch (Exception $e) {
-            throw new Exception('Error Processing Request', 1);
+            throw new Exception('Error initializing Nelisy/Snmp', 1);
         }
     }
-    
+
+    public function setHost($host, $comunity)
+    {
+        try {
+            $this->instance = new Snmp($host, $comunity);
+        } catch (Exception $e) {
+            throw new Exception('Error initializing Nelisy/Snmp', 1);
+        }
+    }
+
     public function get($oids)
     {
         return $this->instance->get($oids);
     }
-    
+
     public function walk($oid)
     {
         return $this->instance->walk($oid);
